29496f5715a5512aba2a14f36bbfaa30180509e3
Henrique de Padua Valle <henrique.valle@escola.pr.gov.br>
2024-10-04 08:39:31 -0300

    atualização

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let btn = document.getElementById('bb');
 function mostra() {
     
 }
+
+let listaProdutos = document.getElementsByClassName("produtos2");
+
+listaProdutos[0].addEventListener('click', addProdutoCarrinho);
+
+function addProdutoCarrinho(){
+    let precoTexto = listaProdutos[0].getElementsByTagName('p').innerText;
+    let precoNumero = precoTexto.slice(' ');
+
+    document.getElementById('carrinho_total').innerText += precoNumero;
+}
